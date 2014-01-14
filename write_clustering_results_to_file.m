cef7d45e2e4fd5b1c5bcf58b7419714318ced0b6
Shuai Li <lishuai918@gmail.com>
2014-01-14 18:36:13 +0800

    Add explanation for write_clustering_results_to_file.m

--- write_clustering_results_to_file.m
+++ write_clustering_results_to_file.m
@@ -1,3 +1,12 @@
+% Input: clusters in cell array format. 
+%       vertex num
+%       filename
+% This function write clustering results into file in the following format:
+% 1 each vertex takes up a row, the num is the group id it belongs to.
+% 2
+% 2
+% ...
+
 function write_clustering_results_to_file(clusters, vertex_num, filename)
     cluster_num         = length(clusters);
     clustering_result   = zeros(1, vertex_num);
